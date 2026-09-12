7383c3e28fe222ce71b9fcf03e14f9f19642de20
Fiorans03 <nicholas.fiorani@studenti.unicam.it>
2026-09-12 15:28:13 +0200

    MainFX corretto

--- src/main/java/it/unicam/cs/mpgc/rpg122756/MainFX.java
+++ src/main/java/it/unicam/cs/mpgc/rpg122756/MainFX.java
@@ -53,7 +53,6 @@ import java.io.File;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
